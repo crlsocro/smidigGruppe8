9d57075c898c71e95cbc4b92163d67b8705a9c35
hagenh <haakonhagen@live.no>
2021-05-12 12:33:11 +0200

    Splashscreen

--- app/src/main/java/com/example/smidig/SplashActivity.kt
+++ app/src/main/java/com/example/smidig/SplashActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         supportActionBar?.hide()
 
         Handler().postDelayed({
-            val intent = Intent(this@MainActivity, MapsActivity::class.java)
+            val intent = Intent(this@SplashActivity, MapsActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
