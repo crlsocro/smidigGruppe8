a51109711c92be996167707b4ccde7a06f422927
hkonn <hkonnhaagaas@gmail.com>
2021-05-12 12:46:17 +0200

    fix

--- app/src/main/java/com/example/smidig/SplashActivity.kt
+++ app/src/main/java/com/example/smidig/SplashActivity.kt
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         Handler().postDelayed({
-            val intent = Intent(this@SplashActivity, MapsActivity::class.java)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
