257b573484a7127141522ec7bd507073ba5d0bb2
hkonn <hkonnhaagaas@gmail.com>
2021-05-12 10:42:18 +0200

    added places

--- app/src/main/java/com/example/smidig/MapsActivity.kt
+++ app/src/main/java/com/example/smidig/MapsActivity.kt
@@ -2,6 +2,9 @@ package com.example.smidig
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smidig.BuildConfig.GOOGLE_MAPS_API_KEY
+import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.Places
 
 class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
