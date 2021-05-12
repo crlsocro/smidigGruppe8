decf0631269ba36cf7acc24a41ff16a3ab9026a7
chris <christian.r.lie@hotmail.no>
2021-05-12 11:06:04 +0200

    maps dep

--- app/src/main/java/com/example/smidig/FrontPage.kt
+++ app/src/main/java/com/example/smidig/FrontPage.kt
@@ -1,10 +1,9 @@
 package com.example.smidig
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-
-class MapsActivity : AppCompatActivity() {
+class FrontPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
--- app/src/main/java/com/example/smidig/LoginActivity.kt
+++ app/src/main/java/com/example/smidig/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.smidig
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smidig.databinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
     }
+    binding.butto
 }
