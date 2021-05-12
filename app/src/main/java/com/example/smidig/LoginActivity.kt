d31bc78065379ea100dd5d7cac16fdbdb62d36b5
hkonn <hkonnhaagaas@gmail.com>
2021-05-12 11:24:17 +0200

    maps funker ish

--- app/src/main/java/com/example/smidig/LoginActivity.kt
+++ app/src/main/java/com/example/smidig/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.smidig
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.smidig.databinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +10,4 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
     }
-    binding.butto
 }
