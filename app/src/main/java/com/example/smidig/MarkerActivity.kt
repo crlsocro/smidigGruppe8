9df0a7a994ec6f62797670b0e1e903d4f1d78868
hkonn <hkonnhaagaas@gmail.com>
2021-05-13 15:02:52 +0200

    map webView

--- app/src/main/java/com/example/smidig/MarkerActivity.kt
+++ app/src/main/java/com/example/smidig/MarkerActivity.kt
@@ -1,11 +1,34 @@
 package com.example.smidig
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MarkerActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marker)
+
+
+        var webView = findViewById<WebView>(R.id.pinMap)
+        webView.setInitialScale(1)
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.allowFileAccess = true
+        webView.settings.pluginState = WebSettings.PluginState.ON
+        webView.settings.pluginState = WebSettings.PluginState.ON_DEMAND
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+
+        webView.loadUrl("https://www.google.com/maps/d/embed?mid=1Xx48rtXPMkjq9f7B7NXAW-gcQ5l7h-AT&hl=en")
+
     }
+
 }
