0c5b4fab829729452517b09eb8230f6260887840
chris <christian.r.lie@hotmail.no>
2021-05-13 12:57:03 +0200

    quizzy

--- app/src/main/java/com/example/smidig/QuizActivity.kt
+++ app/src/main/java/com/example/smidig/QuizActivity.kt
@@ -1,9 +1,11 @@
 package com.example.smidig
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -41,6 +43,12 @@ class QuizActivity: AppCompatActivity() {
         optionTwo.setOnClickListener(this)
         optionThree.setOnClickListener(this)
         optionFour.setOnClickListener(this)*/
+
+        var submitBtn = findViewById<Button>(R.id.submitBtn)
+        submitBtn.setOnClickListener {
+            val i = Intent(this, MapsActivity::class.java)
+            startActivity(i)
+        }
     }
 
 }
