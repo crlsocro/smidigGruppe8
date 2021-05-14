906847ba809459613f13e1b49f4ac5ff5241d636
hagenh <haakonhagen@live.no>
2021-05-14 10:16:13 +0200

    Fixed the back arrow in SummaryActivity.kt

--- app/src/main/java/com/example/smidig/SummaryActivity.kt
+++ app/src/main/java/com/example/smidig/SummaryActivity.kt
@@ -1,11 +1,20 @@
 package com.example.smidig
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SummaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
+        supportActionBar?.hide()
+    }
+
+    fun toMap(view: View) {
+        val intent = Intent(this, MapsActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
