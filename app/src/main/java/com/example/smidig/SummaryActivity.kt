971b8ffe4a6b971b5cc601696be0af8b5654f537
chris <christian.r.lie@hotmail.no>
2021-05-20 10:48:51 +0200

    Ratingbar

--- app/src/main/java/com/example/smidig/SummaryActivity.kt
+++ app/src/main/java/com/example/smidig/SummaryActivity.kt
@@ -4,12 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.Toast
 
 class SummaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
         supportActionBar?.hide()
+
+
+        //Copied from https://www.geeksforgeeks.org/ratingbar-in-kotlin/
+        val rBar = findViewById<RatingBar>(R.id.rBar)
+        if (rBar != null) {
+            val button = findViewById<Button>(R.id.button)
+            button?.setOnClickListener {
+                val msg = rBar.rating.toString()
+                Toast.makeText(this@SummaryActivity,
+                    "Rating is: "+msg, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     fun toMap(view: View) {
