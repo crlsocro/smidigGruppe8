ac435cee5832e0118a742bebf1812edd3d16950a
chris <christian.r.lie@hotmail.no>
2021-05-21 13:18:31 +0200

    History

--- app/src/main/java/com/example/smidig/History/HistoryAdapter.kt
+++ app/src/main/java/com/example/smidig/History/HistoryAdapter.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.smidig.databinding.ItemHistoryViewBinding
-import com.squareup.picasso.Picasso
+
 
 class HistoryAdapter(private var list: List<HistoryStats>): RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
 
