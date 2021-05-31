1a1ac871298e5064146d7365be9498ab8c0b0825
chris <christian.r.lie@hotmail.no>
2021-05-31 12:37:58 +0200

    correct pin to info and Marker

--- app/src/main/java/com/example/smidig/History/InfoActivity.kt
+++ app/src/main/java/com/example/smidig/History/InfoActivity.kt
@@ -68,7 +68,7 @@ class InfoActivity: AppCompatActivity() {
         webView.settings.loadWithOverviewMode = true
         webView.settings.useWideViewPort = true
 
-        webView.loadUrl("https://www.google.com/maps/d/embed?mid=186sevV9ZCwa93pp86Alg8P2sP9mCviI9&ll=59.909377174847734%2C10.767309476730954&z=16")
+        webView.loadUrl("https://www.google.com/maps/d/embed?mid=1JKsxtkyA7Opg17O2bQ0iOGjaGJJKXqJo")
 
         var completeBtn = findViewById<Button>(R.id.completeBtn)
         completeBtn.setOnClickListener{
